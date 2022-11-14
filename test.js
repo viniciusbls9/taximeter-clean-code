71be6e2af263fcb1f512248478d63db170fac0ac
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-11-14 11:21:10 +0000

    test: change test to use Trip class

--- test.js
+++ test.js
@@ -1,21 +1,20 @@
-const taximeter = require('./taximeter')
+const Trip = require('./Trip')
 
 test('should be normal fare', function () {
-  const fare = taximeter.calculateTrip(new Date('2018-10-10T10:00'), 1000)
+  const fare = new Trip(new Date('2018-10-10T10:00'), 1000).calculateTrip()
   expect(fare).toBe(2100)
 })
 
 test('should be sunday fare', function () {
-  const fare = taximeter.calculateTrip(new Date('2018-10-07T10:00'), 1000)
+  const fare = new Trip(new Date('2018-10-07T10:00'), 1000).calculateTrip()
   expect(fare).toBe(3000)
 })
 
 test('should be overnight fare', function () {
-  const fare = taximeter.calculateTrip(new Date('2018-10-10T23:00'), 1000)
+  const fare = new Trip(new Date('2018-10-10T23:00'), 1000).calculateTrip()
   expect(fare).toBe(3900)
 })
 
 test('should be wrong date', function () {
-  const fare = taximeter.calculateTrip('2018-10-10T23:00', 1000)
-  expect(fare).toBe(-1)
+  expect(() => new Trip('2018-10-10T23:00', 1000).calculateTrip()).toThrow(Error)
 })
